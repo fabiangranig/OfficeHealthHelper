ea2897292467e7760522a9e14d902b7b2e2c254a
Fabian G. <fabiangranig.fg@gmail.com>
2024-05-05 21:32:28 +0200

    Added first project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace OfficeHealthHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_Timer = new System.Windows.Forms.Label();
+            this.label_Bodyposition = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_Timer
+            // 
+            this.label_Timer.AutoSize = true;
+            this.label_Timer.Location = new System.Drawing.Point(12, 9);
+            this.label_Timer.Name = "label_Timer";
+            this.label_Timer.Size = new System.Drawing.Size(96, 16);
+            this.label_Timer.TabIndex = 0;
+            this.label_Timer.Text = "<empty Timer>";
+            // 
+            // label_Bodyposition
+            // 
+            this.label_Bodyposition.AutoSize = true;
+            this.label_Bodyposition.Location = new System.Drawing.Point(12, 36);
+            this.label_Bodyposition.Name = "label_Bodyposition";
+            this.label_Bodyposition.Size = new System.Drawing.Size(104, 16);
+            this.label_Bodyposition.TabIndex = 1;
+            this.label_Bodyposition.Text = "<Body Position>";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 221);
+            this.Controls.Add(this.label_Bodyposition);
+            this.Controls.Add(this.label_Timer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Timer;
+        private System.Windows.Forms.Label label_Bodyposition;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace OfficeHealthHelper
+{ 
+    enum BodyPosition
+    {
+        Sitting,
+        Standing
+    }
+
+    public partial class Form1 : Form
+    {
+        private int _DefaultStartTime;
+        private int _TimerCount;
+        private Thread _CountDown;
+        private BodyPosition _BodyPosition;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Set default variables
+            this._DefaultStartTime = 10;
+            this._BodyPosition = BodyPosition.Standing;
+            this.Icon = Icon.ExtractAssociatedIcon("ico/StandingMan.ico");
+            BodyLabelRefresh();
+            this._CountDown = new Thread(ThreadCountDown);
+
+            //Set to Timer to 1 hour
+            this._TimerCount = this._DefaultStartTime;
+
+            //Start thread
+            this._CountDown.Start();
+        }
+
+        private void ThreadCountDown()
+        {
+            while(1 == 1)
+            {
+                TimerLabelRefresh();
+                Thread.Sleep(1000);
+                this._TimerCount--;
+
+                if(this._TimerCount == -1)
+                {
+                    this._TimerCount = this._DefaultStartTime;
+
+                    if(this._BodyPosition == BodyPosition.Sitting)
+                    {
+                        this._BodyPosition = BodyPosition.Standing;
+                        ChangeICON();
+                    }
+                    else if (this._BodyPosition == BodyPosition.Standing)
+                    {
+                        this._BodyPosition = BodyPosition.Sitting;
+                        ChangeICON();
+                    }
+                    BodyLabelRefresh();
+                }
+            }
+        }
+
+        private void TimerLabelRefresh()
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(TimerLabelRefresh));
+            }
+            else
+            {
+                this.label_Timer.Text = this._TimerCount.ToString();
+            }
+        }
+
+        private void ChangeICON()
+        {
+            if(InvokeRequired)
+            {
+                BeginInvoke(new Action(ChangeICON));
+            }
+            else
+            {
+                if (this._BodyPosition == BodyPosition.Sitting)
+                {
+                    this.Icon = Icon.ExtractAssociatedIcon("ico/SittingMan.ico");
+                }
+                else if (this._BodyPosition == BodyPosition.Standing)
+                {
+                    this.Icon = Icon.ExtractAssociatedIcon("ico/StandingMan.ico");
+                }
+            }
+        }
+
+        private void BodyLabelRefresh()
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(BodyLabelRefresh));
+            }
+            else
+            {
+                this.label_Bodyposition.Text = this._BodyPosition.ToString();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this._CountDown != null && this._CountDown.IsAlive)
+            {
+                this._CountDown.Abort();
+            }
+        }
+    }
+}
